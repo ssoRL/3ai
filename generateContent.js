9a3c85349e657a6583388311cb4124336932a34b
Skyler Olson <skyler.olson.rl@gmail.com>
2020-06-30 11:05:59 -0700

    Reconfigure around subfolder for the site

--- generateContent.js
+++ generateContent.js
@@ -1,16 +1,17 @@
 var marked = require('marked');
 var fs = require('fs');
 
+// Generate "The Orthogonal Machines"
 var readOrth = fs.readFileSync('story/orth.md', 'utf-8');
 var orthContent = marked(readOrth);
+fs.writeFileSync('./3ai/story/orth.html', orthContent);
 
-fs.writeFileSync('./story/orth.html', orthContent);
-
+// Generate "Vessels of Perfection"
 var readPerfect = fs.readFileSync('story/perfect.md', 'utf-8');
 var perfectContent = marked(readPerfect);
+fs.writeFileSync('./3ai/story/perfect.html', perfectContent);
 
-fs.writeFileSync('./story/perfect.html', perfectContent);
-
+// Generate "Vines of Kudzu"
 var readKudzu = fs.readFileSync('story/kudzu.md', 'utf-8');
 // Split the ensuing file by kudzusectionbreak
 var kudzuSections = readKudzu.split('\n\nkudzusectionbreak\n\n');
@@ -19,4 +20,4 @@ for(section of kudzuSections) {
     paragraphs = section.split('\n\n');
     kudzuParagraphs.push(paragraphs)
 }
-fs.writeFileSync('./story/kudzu.json', JSON.stringify(kudzuParagraphs));
+fs.writeFileSync('./3ai/story/kudzu.json', JSON.stringify(kudzuParagraphs));
