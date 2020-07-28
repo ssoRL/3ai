23602411942b18ac6b89cb9dac0743c434ae0e9a
Skyler Olson <skyler.olson.rl@gmail.com>
2020-07-28 11:37:27 -0700

    Don't use intervals for canvas animation

--- ts/canvas-util/canvas-controller.ts
+++ ts/canvas-util/canvas-controller.ts
@@ -10,8 +10,24 @@ class CanvasController {
     private scale = 1;
     public offset: Point = {x: 0, y: 0};
     private clickables: Clickable[] = [];
+    /** postion the reset button in the lower right hand corner */
     private reset_height: number;
     private reset_width: number;
+    /** When the current animation started */
+    private animation_start_time: number;
+    /** When the current animation ends */
+    private animation_final_time: number;
+    /** How long the animation lasts for */
+    private animation_total_duration: number;
+    /** The offset that the current animation is starting at */
+    private animation_start_offset: Point;
+    /** How much the animation shifts in the x and y direction */
+    private animation_dx: number;
+    private animation_dy: number;
+    /** The easing function to animate with */
+    private animation_easing_function: (input: number) => number;
+    /** the function to resolve the animation promise, or undefined if there is no animation happening  */
+    private resolveAnimation: (() => void) | undefined = undefined;
 
     constructor(canvas_: HTMLCanvasElement, canvas_container_: HTMLDivElement){
         this.canvas = canvas_;
@@ -37,37 +53,50 @@ class CanvasController {
         })
     }
 
+    public checkAnimationStatus() {
+        if (this.resolveAnimation !== undefined) {
+            // There is a animation happening now
+            if(glb.time > this.animation_final_time) {
+                // The animation is completed, end it
+                this.offset = p(
+                    this.animation_start_offset.x + this.animation_dx,
+                    this.animation_start_offset.y + this.animation_dy
+                );
+                this.resolveAnimation();
+                this.resolveAnimation = undefined;
+            } else {
+                // set the offset correctly based on how long the animation has run
+                const animation_duration = glb.time - this.animation_start_time;
+                const duration_frac = animation_duration / this.animation_total_duration;
+                const eased_frac = this.animation_easing_function(duration_frac);
+                this.offset = p(
+                    this.animation_start_offset.x + eased_frac*this.animation_dx,
+                    this.animation_start_offset.y + eased_frac*this.animation_dy
+                );
+            }
+        }
+    }
+
     public async animateTranslate(
         x: number, y: number, duration: number,
         easing = (t: number) => t
     ) {
-        const from = {x: this.offset.x, y: this.offset.y};
-        const dx = x - from.x;
-        const dy = y - from.y;
-        const animation_start_time = performance.now();
+        if(this.resolveAnimation !== undefined) {
+            // only set one animation at a time
+            throw "3AI Error: Only may have one global canvas animation running at a time.";
+        }
 
-        const interval_handle = window.setInterval(
-            () => {
-                const t = glb.time;
-                const fraction = (t - animation_start_time) / duration;
-                const ease_frac = easing(fraction);
-                this.offset = {
-                    x: from.x + dx*ease_frac,
-                    y: from.y + dy*ease_frac
-                }
-            },
-            10
-        );
+        // Set up values to track the animation
+        this.animation_start_time = glb.time;
+        this.animation_final_time = glb.time + duration;
+        this.animation_total_duration = duration;
+        this.animation_start_offset = this.offset;
+        this.animation_dx = x - this.offset.x;
+        this.animation_dy = y - this.offset.y;
+        this.animation_easing_function = easing;
 
         return new Promise((resolve) => {
-            window.setTimeout(
-                () => {
-                    window.clearInterval(interval_handle);
-                    this.offset = {x: x, y: y};
-                    resolve();
-                },
-                duration
-            );
+            this.resolveAnimation = resolve;
         })
     }
 
